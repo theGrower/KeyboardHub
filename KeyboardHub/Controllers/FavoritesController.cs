664120c02c72593a9bb506c31757558365ab3a2a
Jeff Stewart <jeff.stewart@protonmail.com>
2017-06-05 12:34:20 -0400

    Scaffolded out our custom elements.

---
+++ KeyboardHub/Controllers/FavoritesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KeyboardHub.Models;
+
+namespace KeyboardHub.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private KeyboardHubDB db = new KeyboardHubDB();
+
+        // GET: Favorites
+        public async Task<ActionResult> Index()
+        {
+            var favorites = db.Favorites.Include(f => f.AspNetUser1);
+            return View(await favorites.ToListAsync());
+        }
+
+        // GET: Favorites/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Favorite favorite = await db.Favorites.FindAsync(id);
+            if (favorite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(favorite);
+        }
+
+        // GET: Favorites/Create
+        public ActionResult Create()
+        {
+            ViewBag.AspNetUser = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Favorites/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "FavoriteID,C_URL,AspNetUser")] Favorite favorite)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Favorites.Add(favorite);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AspNetUser = new SelectList(db.AspNetUsers, "Id", "Email", favorite.AspNetUser);
+            return View(favorite);
+        }
+
+        // GET: Favorites/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Favorite favorite = await db.Favorites.FindAsync(id);
+            if (favorite == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AspNetUser = new SelectList(db.AspNetUsers, "Id", "Email", favorite.AspNetUser);
+            return View(favorite);
+        }
+
+        // POST: Favorites/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "FavoriteID,C_URL,AspNetUser")] Favorite favorite)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(favorite).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AspNetUser = new SelectList(db.AspNetUsers, "Id", "Email", favorite.AspNetUser);
+            return View(favorite);
+        }
+
+        // GET: Favorites/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Favorite favorite = await db.Favorites.FindAsync(id);
+            if (favorite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(favorite);
+        }
+
+        // POST: Favorites/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Favorite favorite = await db.Favorites.FindAsync(id);
+            db.Favorites.Remove(favorite);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ KeyboardHub/Controllers/SwitchesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KeyboardHub.Models;
+
+namespace KeyboardHub.Controllers
+{
+    public class SwitchesController : Controller
+    {
+        private KeyboardHubDB db = new KeyboardHubDB();
+
+        // GET: Switches
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Switches.ToListAsync());
+        }
+
+        // GET: Switches/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Switch @switch = await db.Switches.FindAsync(id);
+            if (@switch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@switch);
+        }
+
+        // GET: Switches/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Switches/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "SwitchID,Color,Type,AccuationForce,Description,TouchAudio,TouchOringAudio,BottomAudio,BottomOringAudio,GIFPath")] Switch @switch)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Switches.Add(@switch);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(@switch);
+        }
+
+        // GET: Switches/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Switch @switch = await db.Switches.FindAsync(id);
+            if (@switch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@switch);
+        }
+
+        // POST: Switches/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "SwitchID,Color,Type,AccuationForce,Description,TouchAudio,TouchOringAudio,BottomAudio,BottomOringAudio,GIFPath")] Switch @switch)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@switch).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(@switch);
+        }
+
+        // GET: Switches/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Switch @switch = await db.Switches.FindAsync(id);
+            if (@switch == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@switch);
+        }
+
+        // POST: Switches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Switch @switch = await db.Switches.FindAsync(id);
+            db.Switches.Remove(@switch);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
